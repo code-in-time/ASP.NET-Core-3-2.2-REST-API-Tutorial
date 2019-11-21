ed0c55b123999cff510d3cfb034be0d5ddc8e70b
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2019-11-21 21:22:25 +0200

    Creating resources using POST | ASP.NET Core 3 & 2.2 REST API Tutorial 4

--- Tweetbook/Contracts/V1/ApiRoutes.cs
+++ Tweetbook/Contracts/V1/ApiRoutes.cs
@@ -13,7 +13,9 @@ namespace Tweetbook.Contracts.V1
 
         public static class Posts
         {
-            public const  string GetAll = Base + "/posts";
+            public const string GetAll = Base + "/posts";
+            public const string Get = Base + "/posts/{postId}";
+            public const string Create = Base + "/posts";
         }
     }
 }
--- Tweetbook/Controllers/V1/PostsController.cs
+++ Tweetbook/Controllers/V1/PostsController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Tweetbook.Contracts.V1;
+using Tweetbook.Controllers.V1.Requests;
+using Tweetbook.Controllers.V1.Response;
 using Tweetbook.Domain;
 
 namespace Tweetbook.Controllers
@@ -28,6 +30,23 @@ namespace Tweetbook.Controllers
         public IActionResult GetAll()
         {
             return Ok(_posts);
+        }
+
+        [HttpPost(ApiRoutes.Posts.Create)]
+        public IActionResult Create([FromBody] CreatePostRequest postRequest)
+        {
+
+            var post = new Post { Id = postRequest.Id };
+            if (string.IsNullOrEmpty(post.Id))
+                post.Id = Guid.NewGuid().ToString();
+            //return Ok(_posts[1].Id);
+            _posts.Add(post);
+
+            var baseUrl = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host.ToUriComponent()}";
+            var locationUri = baseUrl + "/" + ApiRoutes.Posts.Get.Replace("{postId}", post.Id);
+
+            var response = new PostResponse { Id = post.Id };
+            return Created(locationUri, post);
         }
     }
 }
